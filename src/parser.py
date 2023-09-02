efd6a6430f070a416ef2ceeaeef758314b0fbf57
Danielk <74910071+danielk-kruger@users.noreply.github.com>
2023-09-02 12:01:48 -0300

    Added file system Algorithms - need reviews

--- src/parser.py
+++ src/parser.py
@@ -4,44 +4,72 @@ import os, os.path
 
 class Parser
 	file_paths = []
+	dir_path = "data"
 
 	def __init__(self):
 		self.init_datasets()
 
 
+	# Here we get all our datasets and store into an array of dictionaries
+	# This way we can distinguish what is a file and what isn`t.
+	
+	# base data structure.
+	# file_paths -> [
+	# 	'comms.json',
+	# 	'settings.json',
+	# 	{ 'contacts': { 'test_contacts.csv': 'data/contacts/test_contacts.csv' }}
+	#	... etc
+	# ]
 	def init_datasets(self):
-		dir_path = "data"
 		for entry in os.listdir(dir_path):				
 			if os.path.isdir(entry):
+				entries = [self.filter_entries(entry)]
 				self.set_filepath({ 
-					os.path.basename(entry): [file for file in entry if self.is_dataset(file)]
+					self.get_name(entry): [{ 
+						self.get_name(deep_entry): deep_entry 
+					} for deep_entry in entries]
 				})
 			
 			self.set_filepath(os.path.join(dir_path, entry))
 
 	def set_filepath(self, file):
-		print(f"Found file: {os.basename(file)}")
+		print(f"Found file: {self.get_name(file)}")
 		self.file_paths.append(file)
+
+	def filter_entries(self, entry):
+		return [os.path.join(self.dir_path, file) for file in entry if self.is_dataset(file)]
 
 	def get_filepaths(self):
 		return self.file_paths
+
+	def get_name(self, file_path):
+		return os.path.basename(file_path)
 
 	def is_dataset(self, data_file):
 		return entry.endswith('.csv') or entry.endswith('.json')
 
 
 class Contacts(Parser):
-	list = []
+	# list = []
 
-	def __init__(self):
-		self.get_contactlists()
+	def __init__(self, contact, list=[]):
+		# self.list = []
+		self.list = self.get_contactlists()
+		self.contact = contact if contact is not '' else self.list[0]
+
 
 	def get_contactlists(self):
 		for item in self.get_filepaths():
-			if os.path.isdir(item) and item == 'contacts':
-				self.list = os.listdir(item)
+			contacts = item['contacts'].keys()
+			print(contacts)
 
-	def parse_data(self):
+	# Return the file name if it is inside 
+	def contact_selecto(self, file_name):
+		names = [self.get_name(file) for file in self.list]
+		if file_name in names:
+			return file_name
+
+	def parse_contacts(self):
 		df = pd.read_csv(file, delimiter=";", encoding='latin1')
 		data = []
 
@@ -54,9 +82,11 @@ class Contacts(Parser):
 
 		return data
 
+	def set_list(self, new_list):
+		self.list = new_list
+
 class Comms(Parser):
 	options = []
-	comms_file = "data/comms.json"
 
 	def get_comms(self):
 		options = []
